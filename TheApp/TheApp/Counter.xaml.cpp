0512312bc0c8f43ea2e71c9c9435573412cd38ff
Dorian Cortes <19631146+doriandres@users.noreply.github.com>
2025-06-18 00:24:55 -0600

    Removed unnecessary usings

--- TheApp/TheApp/Counter.xaml.cpp
+++ TheApp/TheApp/Counter.xaml.cpp
@@ -4,22 +4,13 @@
 #include "CounterViewModel.h"
 
 using namespace Platform;
-using namespace Windows::Foundation;
-using namespace Windows::Foundation::Collections;
-using namespace Windows::UI::Xaml;
-using namespace Windows::UI::Xaml::Controls;
-using namespace Windows::UI::Xaml::Controls::Primitives;
-using namespace Windows::UI::Xaml::Data;
-using namespace Windows::UI::Xaml::Input;
-using namespace Windows::UI::Xaml::Media;
-using namespace Windows::UI::Xaml::Navigation;
 
 namespace TheApp
 {
 	Counter::Counter()
 	{
 		InitializeComponent();
-		m_viewModel = ref new CounterViewModel();		
+		m_viewModel = ref new CounterViewModel();
 	}
 
 	CounterViewModel^ Counter::ViewModel::get()
@@ -29,11 +20,11 @@ namespace TheApp
 
 	void Counter::InitialCount::set(int value)
 	{
-		m_viewModel->Count = value;		
+		m_viewModel->Count = value;
 	}
 
 	void Counter::BtnLabel::set(String^ value)
-	{	
+	{
 		m_viewModel->IncreaseBtnLabel = value;
 	}
 }
